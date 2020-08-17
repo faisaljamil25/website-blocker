22bd5bcc3ab29a5d694ac81c4a0a9b51110e4805
Faisal Jamil <faisaljamilcs@gmail.com>
2020-08-17 23:23:19 +0530

    Websites list from file

--- website_blocker.py
+++ website_blocker.py
@@ -3,7 +3,8 @@ from datetime import datetime as dt
 
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
-website_list = ["www.facebook.com", "facebook.com", "outlook.live.com"]
+with open("websites.txt", 'r') as file:
+    website_list = file.readlines()
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month,
